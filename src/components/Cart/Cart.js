3efeb58eefa0296904fbd435f531a92cb1551f30
Easrib <easrib.sharik01@gmail.com>
2022-03-25 19:14:24 +0600

    cart updated-01

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './Cart.css'
 
-const Cart = (props) => {
+const Cart = ({ cart }) => {
+
+    console.log(cart);
+
+    let total = 0;
+    let shipping = 0;
+
+    for (const product of cart) {
+
+        total = total + product.price
+        shipping = shipping + product.shipping;
+    }
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax;
     return (
-        <div>
+        <div className='cart'>
             <h1>Order Summery</h1>
-            <h3>Selected Items: {props.cart.length}</h3>
+            <h3>Selected Items: {cart.length}</h3>
+            <p>Total Price:$ {total} </p>
+            <p>Total Shipping Charge:$ {shipping}</p>
+            <p>Tax:$ {tax}</p>
+            <h2>Grand Total: $ {grandTotal}</h2>
         </div>
     );
 };
