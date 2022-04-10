f5736aad6aba02a0b2cb7300475108c497527c4d
Easrib <easrib.sharik01@gmail.com>
2022-04-11 05:23:18 +0600

    react router set

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/Header';
+import Login from './components/Login/Login';
 import Shop from './components/shop/Shop';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Routes>
+        <Route path='/' element={<Shop></Shop>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
